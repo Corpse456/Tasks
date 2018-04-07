0c46d16edef1c6d1417a322b841f13e3d62e0ee5
Alexander Neznaev <Corpsetrup@gmail.com>
2018-04-07 09:26:50 +0300

    SavingUniverse

---
+++ src/main/java/googleJam/savingUniverse/Solution.java
@@ -0,0 +1,16 @@
+
+
+public class Solution {
+	public static void main(String[] args) {
+        String program = "SCCSSC";
+        int damage = calculateDamage(program.toCharArray());
+    }
+
+    private static int calculateDamage(char ...program) {
+        int damage = 0;
+        int strength = 1;
+       
+        
+        return damage;
+    }
+}
