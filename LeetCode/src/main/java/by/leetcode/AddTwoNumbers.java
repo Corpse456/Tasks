0ddd8463e217f97907c8333b0ccb5179ccf9946a
Alexander Neznaev <corpsetrup@gmail.com>
2023-04-25 18:55:36 +0300

    AddTwoNumbers solved

--- LeetCode/src/main/java/by/leetcode/AddTwoNumbers.java
+++ LeetCode/src/main/java/by/leetcode/AddTwoNumbers.java
@@ -3,6 +3,30 @@ package by.leetcode;
 public class AddTwoNumbers {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return new ListNode();
+        var result = new ListNode();
+        var currentNode = result;
+        while (l1 != null || l2 != null) {
+            var initialVal = 0;
+            var sum = currentNode.val + getValue(l1) + getValue(l2);
+            if (sum - 10 >= 0) {
+                currentNode.val = sum - 10;
+                initialVal = 1;
+            } else {
+                currentNode.val = sum;
+            }
+            if (initialVal != 0 || (l1 != null && l1.next != null) || (l2 != null && l2.next != null)) {
+                currentNode.next = new ListNode(initialVal);
+                currentNode = currentNode.next;
+            }
+            l1 = l1 != null ? l1.next : null;
+            l2 = l2 != null ? l2.next : null;
+        }
+
+        return result;
     }
+
+    private int getValue(final ListNode node) {
+        return node != null ? node.val : 0;
+    }
+
 }
